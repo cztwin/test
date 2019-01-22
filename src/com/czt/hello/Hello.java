df80c7fd4789f7bb6924249c9e5c1e39c2cf3883
cztwin <760567691@qq.com>
2019-01-22 15:27:05 +0800

    git Test

--- src/com/czt/hello/Hello.java
+++ src/com/czt/hello/Hello.java
@@ -2,6 +2,6 @@ package com.czt.hello;
 
 public class Hello {
         public static void main(String[] args){
-            System.out.println("hello World!!!");
+            System.out.println("hello World!!!!!!");
         }
 }
